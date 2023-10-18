3ceb10f85563698b4d3a9f076616667234bbccf0
ThVbs <r2dti265@gmail.com>
2023-10-18 20:41:00 -0300

    Done

---
+++ js/portifolio.js
@@ -0,0 +1,13 @@
+<script>
+  $(document).ready(function(){
+    $('.slick-carousel').slick({
+      autoplay: true, // defina como true para o carrossel passar automaticamente
+      arrows: false, // defina como true para mostrar as setas de navegação
+      dots: true, // defina como true para mostrar os pontos de navegação
+      infinite: true, // defina como true para permitir navegação infinita
+      slidesToShow: 3, // defina quantos slides são mostrados ao mesmo tempo
+      slidesToScroll: 1 // defina quantos slides são movidos quando navegando
+    })
+  } );
+</script>
+
